8e47b5a4fe5fda48d027446c1becc5c27ac95498
Alessandro D'Aquino (elara) <alex@midokura.com>
2014-11-01 10:32:22 +0100

    Added ember-table styles, works when used as addon
    Signed-off-by: Alessandro D'Aquino (elara) <alex@midokura.com>

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    //app.import(app.bowerDirectory + '/ember-table/dist/ember-table.css');
+    app.import('vendor/ember-table.css');
     app.import(app.bowerDirectory + '/antiscroll/antiscroll.css');
-    // Make this work
-    //app.import('app/styles/ember-table.css');
 
     app.import(app.bowerDirectory + '/antiscroll/antiscroll.js');
     app.import(app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js');
@@ -17,6 +15,5 @@ module.exports = {
     app.import(app.bowerDirectory + '/jquery-ui/ui/jquery.ui.mouse.js');
     app.import(app.bowerDirectory + '/jquery-ui/ui/jquery.ui.resizable.js');
     app.import(app.bowerDirectory + '/jquery-ui/ui/jquery.ui.sortable.js');
-    //app.import(app.bowerDirectory + '/ember-table/dist/ember-table.js');
   }
 };
