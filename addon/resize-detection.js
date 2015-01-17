8d7229f20ae0efbabf910b6142d0d578d3dde8f1
Jonathan Evans <contact@jrhe.co.uk>
2015-01-17 20:44:02 +0000

    Lint resize-detection code

--- addon/resize-detection.js
+++ addon/resize-detection.js
@@ -1,3 +1,4 @@
+// Private / Helper Methods
 var attachEvent = document.attachEvent;
 var isIE = navigator.userAgent.match(/Trident/);
 var requestFrame = (function(){
@@ -6,15 +7,17 @@ var requestFrame = (function(){
   return function(fn){ return raf(fn); };
 })();
 
-var cancelFrame = (function(){
+var cancelFrame = (function() {
   var cancel = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame ||
          window.clearTimeout;
   return function(id){ return cancel(id); };
 })();
 
-function resizeListener(e){
+function resizeListener(e) {
   var win = e.target || e.srcElement;
-  if (win.__resizeRAF__) cancelFrame(win.__resizeRAF__);
+  if (win.__resizeRAF__) {
+    cancelFrame(win.__resizeRAF__);
+  }
   win.__resizeRAF__ = requestFrame(function(){
     var trigger = win.__resizeTrigger__;
     trigger.__resizeListeners__.forEach(function(fn){
@@ -23,12 +26,14 @@ function resizeListener(e){
   });
 }
 
-function objectLoad(e){
+function objectLoad() {
   this.contentDocument.defaultView.__resizeTrigger__ = this.__resizeElement__;
   this.contentDocument.defaultView.addEventListener('resize', resizeListener);
 }
 
-var addResizeListener = function(element, fn){
+// Public methods
+
+var addResizeListener = function(element, fn) {
   if (!element.__resizeListeners__) {
     element.__resizeListeners__ = [];
     if (attachEvent) {
@@ -36,29 +41,36 @@ var addResizeListener = function(element, fn){
       element.attachEvent('onresize', resizeListener);
     }
     else {
-      if (getComputedStyle(element).position == 'static') element.style.position = 'relative';
-      var obj = element.__resizeTrigger__ = document.createElement('object'); 
+      if (getComputedStyle(element).position === 'static') {
+        element.style.position = 'relative';
+      }
+      var obj = element.__resizeTrigger__ = document.createElement('object');
       obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;');
       obj.__resizeElement__ = element;
       obj.onload = objectLoad;
       obj.type = 'text/html';
-      if (isIE) element.appendChild(obj);
+      if (isIE) {
+        element.appendChild(obj);
+      }
       obj.data = 'about:blank';
-      if (!isIE) element.appendChild(obj);
+      if (!isIE) {
+        element.appendChild(obj);
+      }
     }
   }
   element.__resizeListeners__.push(fn);
 };
 
-var removeResizeListener = function(element, fn){
+var removeResizeListener = function(element, fn) {
   element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1);
   if (!element.__resizeListeners__.length) {
-    if (attachEvent) element.detachEvent('onresize', resizeListener);
-    else {
+    if (attachEvent) {
+      element.detachEvent('onresize', resizeListener);
+    } else {
       element.__resizeTrigger__.contentDocument.defaultView.removeEventListener('resize', resizeListener);
       element.__resizeTrigger__ = !element.removeChild(element.__resizeTrigger__);
     }
   }
-}
+};
 
 export { addResizeListener, removeResizeListener };
