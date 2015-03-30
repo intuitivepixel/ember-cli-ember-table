273eb12d81fc450cfe9fb4cf2d5680581fe2b584
Taras Mankovski <tarasm@gmail.com>
2015-03-30 17:19:55 -0400

    Added ember-try to test compatibility with Ember 1.10.0 & 1.11.0

---
+++ config/ember-try.js
@@ -0,0 +1,16 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'ember-1.10',
+      dependencies: {
+        "ember": "1.10.0"
+      }
+    },
+    {
+      name: 'ember-1.11.0',
+      dependencies: {
+        "ember": "1.11.0"
+      }
+    }
+  ]
+}
