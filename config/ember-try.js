bdf531e529ae754631c05f6980fe89253d99aa1b
Taras Mankovski <tarasm@gmail.com>
2015-04-05 16:22:03 -0400

    Upgraded Ember dependency to Ember 1.11.1

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,15 +1,15 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.10',
+      name: 'ember-1.10.0',
       dependencies: {
         "ember": "1.10.0"
       }
     },
     {
-      name: 'ember-1.11.0',
+      name: 'ember-1.11.1',
       dependencies: {
-        "ember": "1.11.0"
+        "ember": "1.11.1"
       }
     }
   ]
